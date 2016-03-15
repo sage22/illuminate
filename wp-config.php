0c70720a086d52529405859ae3ca789305ca0167
root <root@ip-172-31-54-254.ec2.internal>
2016-03-15 20:35:39 +0000

    Automate Wordpress Database Connection

--- wp-config.php
+++ wp-config.php
@@ -4,7 +4,8 @@
  * The base configuration for WordPress
  *
  * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
+ * installation; however, has been customized by SudirlayCoders. 
+ * You don't have to use the web site, you can
  * copy this file to "wp-config.php" and fill in the values.
  *
  * This file contains the following configurations:
