614bfd956a9968df63c6fab90597dc5ae62292da
root <root@ip-172-31-54-254.ec2.internal>
2016-03-15 19:09:25 +0000

    Automated database removal

---
+++ phpremovemysql.sh
@@ -0,0 +1,37 @@
+<?php
+// This script is a php script that can read any mysql statements into a database_name
+// written to read a file in the same root folder "setusername.sql" and reset all the usernames for admin accounts for mysql
+//Written by: Matthew Trotter --
+
+
+$database_name = 'db_name';
+$sqlFileToExecute = 'setusername.sql';
+$hostname = 'localhost';
+$db_username = 'root';
+$db_pass = 'password';
+$link = mysql_connect($hostname, $db_username, $db_pass);
+
+
+
+if (!$link) {
+  die ("MySQL Connection error");
+}
+
+mysql_select_db($database_name, $link) or die ("Wrong MySQL Database");
+
+// read the sql file
+$f = fopen($sqlFileToExecute,"r+");
+$sqlFile = fread($f, filesize($sqlFileToExecute));
+$sqlArray = explode(';',$sqlFile);
+foreach ($sqlArray as $stmt) {
+  if (strlen($stmt)>3 && substr(ltrim($stmt),0,2)!='/*') {
+    $result = mysql_query($stmt);
+    if (!$result) {
+      $sqlErrorCode = mysql_errno();
+      $sqlErrorText = mysql_error();
+      $sqlStmt = $stmt;
+      break;
+      }
+}
+}
+
