24fd839760b6bd43c4273074f14800283af2a90e
root <root@ip-172-31-54-254.ec2.internal>
2016-03-15 10:27:40 +0000

    Automated Wordpress installation

--- wordpress_install.sh
+++ wordpress_install.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 
+php phpentermysql.sh
+
+host=host_placeholder
+
+wget -c  wget https://wordpress.org/latest.tar.gz
+tar -zxvf latest.tar.gz
+
+/bin/rm latest.tar.gz
+
+mv wordpress /var/www/$host
+
+mv /var/www/$host/wp-config-sample.php wp-config.php
+
+echo "
+
+<VirtualHost *:80>
+        ServerName www.$host.sudirlaycoders.com
+        Serveralias $host.sudirlaycoders.com
+        DocumentRoot /var/www/$host/
+        <Directory /var/www/$host/>
+                Options FollowSymLinks
+                AllowOverride All
+                Order allow,deny
+                Allow from all
+        </Directory>
+</VirtualHost>" >> /etc/httpd/sites-enabled/default.conf
+
+service httpd reload
+
