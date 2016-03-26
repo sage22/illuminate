913aa63dc6c2b3709a13939212378e28d71fb279
root <root@ip-172-31-54-254.ec2.internal>
2016-03-26 11:17:14 +0000

    Automated Drupal Installation

--- drupal_installation.sh
+++ drupal_installation.sh
@@ -25,9 +25,8 @@ cp -fr /tmp/settings.php /var/www/$host/sites/default/
 chmod 777 /var/www/$host/sites/default/settings.php 
 
 else
-
-ls /tmp/* > log.txt
-unzip /tmp/"$theme".zip
+mkdir -p /var/www/$host
+unzip /tmp/"$theme".zip -d /var/www/$host
 /bin/rm /tmp/"$theme".zip
 mv /tmp/$theme /var/www/$host
 chown -R apache:apache /var/www/$host/
